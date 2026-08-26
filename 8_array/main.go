77b09e8b09f0da785f6f9c18c77d4f3a6b32d1e4
Mantra <mantraantani@gmail.com>
2026-08-26 20:15:50 +0530

    Small changes and notes for Array

--- 8_array/main.go
+++ 8_array/main.go
@@ -9,8 +9,18 @@ func main() {
 
 	nums[0] = 1
 	fmt.Println(nums[0]) //returns 1
-	fmt.Println(nums) //returns [1 0 0 0] because 0 is default value for int, false is default for bool and empty string for strings
+	fmt.Println(nums)    //returns [1 0 0 0] because 0 is default value for int, false is default for bool and empty string for strings
 
-	nums1 := [3]int{1,2,3}
+	//1D Array
+	nums1 := [3]int{1, 2, 3}
 	fmt.Println(nums1)
+
+	//2D Array
+	nums2 := [2][2]int{{1, 2}, {3, 4}}
+	fmt.Println(nums2)
+
+	//ADVANTAGES
+	//- fixed size, that is predictable
+	//- Memory optimization
+	//- Constant time access
 }
