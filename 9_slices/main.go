512cc2f7f03deed73ced5a1f45b8dd69830dd663
Mantra <mantraantani@gmail.com>
2026-09-02 19:43:02 +0530

    Final Slice commit

--- 9_slices/main.go
+++ 9_slices/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
-//most used construct in go
-func main(){
+// most used construct in go
+func main() {
 
 	//Uninitialised slice is Nil
 	var nums []int
@@ -26,15 +29,54 @@ func main(){
 
 	var nums2 = make([]int, 2, 5)
 	nums2 = append(nums2, 1)
-	fmt.Println(nums2) //Prints [0 0 1]
+	fmt.Println(nums2)      //Prints [0 0 1]
 	fmt.Println(cap(nums2)) //prints 5
 
 	//if we append more
 	nums2 = append(nums2, 2)
 	nums2 = append(nums2, 3)
 	nums2 = append(nums2, 4)
-	fmt.Println(cap(nums2))//prints 10
-	fmt.Println(nums2) //Prints [0 0 1 2 3 4]
+	fmt.Println(cap(nums2)) //prints 10
+	fmt.Println(nums2)      //Prints [0 0 1 2 3 4]
 
 	//Thus second argument of "make" initialises number of zeroes.
+
+	//Another way to initialise Slice
+
+	num := []int{} //num := []int{1,2,3}
+	fmt.Println(num)
+	fmt.Println(cap(num))
+	fmt.Println(len(num))
+	//Prints [] 0 0
+	num = append(num, 1)
+	//Prints [1] 1 1
+
+	//Using index
+	nums[0] = 3 //Might give error if index is more than the length entered in make
+	//Prints [3 0] if length is 2, first we entered and second is zero by default
+
+	//-----Copy Function------
+	var copy1 = make([]int, 0, 5)
+	copy1 = append(copy1, 2)
+	var copy2 = make([]int, len(copy1))
+
+	//copy1 = append(copy1, 2)
+	fmt.Println(copy1, copy2) //Prints [2] []
+
+	copy(copy2, copy1) //we need to append first then initialise second slice using "len(copy1)"
+	//now prints [2] [2]
+
+	//-----Slicing indices-----
+
+	xyz := []int{1, 2, 3, 4, 5}
+	fmt.Println(xyz[0:2]) //excludes last index i.e. 2
+	//prints [1 2]
+
+	//---Slices package---
+	var abc = []int{1, 2}
+	var def = []int{1, 2}
+	fmt.Println(slices.Equal(abc, def)) //returns true
+
+	//---2D Slices---
+	var twoD = [][]int{{1,2}, {3,4}} //prints [[1 2] [3 4]]
 }
