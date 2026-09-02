af474b6ee172c8a992fba2ac828293c80471de6d
Mantra <mantraantani@gmail.com>
2026-09-02 19:28:10 +0530

    Slices 2

--- 9_slices/main.go
+++ 9_slices/main.go
@@ -13,4 +13,28 @@ func main(){
 	fmt.Println(len(nums))
 	//prints "0" because it is nil
 
+	//--------------------------------
+
+	var nums1 = make([]int, 2)
+	fmt.Println(nums1) //prints "[0 0]"
+	//first argument is datatype
+	//second argument is initial length
+	//third argument is initial capacity, if not mentioned then initial capacity = length
+	fmt.Println(cap(nums))
+	//prints "2"
+	//Cap -> Capacity -> Maximum numbers of elements can fit
+
+	var nums2 = make([]int, 2, 5)
+	nums2 = append(nums2, 1)
+	fmt.Println(nums2) //Prints [0 0 1]
+	fmt.Println(cap(nums2)) //prints 5
+
+	//if we append more
+	nums2 = append(nums2, 2)
+	nums2 = append(nums2, 3)
+	nums2 = append(nums2, 4)
+	fmt.Println(cap(nums2))//prints 10
+	fmt.Println(nums2) //Prints [0 0 1 2 3 4]
+
+	//Thus second argument of "make" initialises number of zeroes.
 }
